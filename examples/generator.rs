d9c8f67091ba854f82552aca385499280363e095
knonderful <knonderful@hotmail.com>
2025-01-09 15:52:31 +0100

    Make example true fibonacci.

--- examples/generator.rs
+++ examples/generator.rs
@@ -2,7 +2,13 @@ use smog::portal::output::{OutBack, OutEvent};
 use smog::{create_driver, CoroPoll};
 use std::pin::pin;
 
-async fn generate(mut portal: OutBack<u8>) {
+async fn fibonacci(mut portal: OutBack<u8>) {
+    // Fibonacci is a good example of the power of coroutines, since this "anomaly" at the start would require a lot
+    // more complex code with "normal" code.
+    for _ in 0..2 {
+        portal.send(0).await;
+    }
+
     let mut a: u8 = 0;
     let mut b: u8 = 1;
 
@@ -21,7 +27,7 @@ async fn generate(mut portal: OutBack<u8>) {
 
 fn main() {
     // The driver is what allows us to both advance the state machine and receive events from the coroutine.
-    let mut driver = pin!(create_driver(generate));
+    let mut driver = pin!(create_driver(fibonacci));
 
     while let CoroPoll::Event(portal_event) = driver.poll() {
         match portal_event {
